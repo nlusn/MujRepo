23fd6bf782b103c41d7224d9f2a82867d5ebcffe
nlusn <abobafettq@gmail.com>
2025-01-09 21:03:25 +0500

    Add files via upload

---
+++ untitled/OOP/Task2.java
@@ -0,0 +1,10 @@
+public class Task2 {
+    public static void main(String[] args) {}
+
+    public class Terran{ }
+
+    public class Human extends Terran{}
+
+    public class Woman extends Human{}
+
+}
---
+++ untitled/OOP/Task3.java
@@ -0,0 +1,19 @@
+public class Task3 {
+    public static void main(String[] args) {}
+
+    public class vehicle{
+        double maxSpeed;
+    }
+
+    public class car extends vehicle{
+        int wheelCount;
+        double weight;
+    }
+
+    public class electricCar extends car{
+        int electricEnginePower;
+    }
+
+
+
+}
