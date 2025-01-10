71aa4909c51c5febf260d344e2710beefebdbe77
nlusn <abobafettq@gmail.com>
2025-01-10 09:12:33 +0600

    Update Task3.java

--- untitled/OOP/Task3.java
+++ untitled/OOP/Task3.java
@@ -1,19 +1,18 @@
-public class Task3 {
-    public static void main(String[] args) {}
+public class Task3 {}
 
-    public class vehicle{
-        double maxSpeed;
-    }
-
-    public class car extends vehicle{
-        int wheelCount;
-        double weight;
-    }
-
-    public class electricCar extends car{
-        int electricEnginePower;
-    }
-
-
-
+public class vehicle{
+    double maxSpeed;
 }
+
+public class car extends vehicle{
+    int wheelCount;
+    double weight;
+}
+
+public class electricCar extends car{
+    int electricEnginePower;
+}
+
+
+
+
